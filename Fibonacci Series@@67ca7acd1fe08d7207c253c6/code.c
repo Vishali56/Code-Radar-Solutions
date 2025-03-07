69c2748f1d95cbb67fa1af8913f4c98d7a2d1dbc
Vishali <vishalijindal185@gmail.com>
2025-03-07 20:21:13 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,23 @@
+#include<tsdio.h>
+void fibbonacciSeries(int n){
+    int first=0,second=1,next;
+    printf("fibonacci series:");
+    for(i=0;i<n;i++){
+       printf("%d",first);
+       next=first+second;
+       first=second;
+       second=next;
+    }
+    printf("\n");
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n<=0){
+        printf("Invalid input\n");
+        return 0;
+    }
+    fibonacciSeries(n);
+    return 0;
+}
